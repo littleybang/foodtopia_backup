25f684ab6d07ae236c26b26005c16c67a179126e
yvonne <yvonne250003@gmail.com>
2018-11-16 18:56:47 +0800

    add product_slider

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import { BrowserRouter, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 import "./App.css";
+
 import Nav from "./components/nav/Nav.js";
 import Loginform from './components/logInform/Loginform.js';
 import Head_slider from './components/head_slider/head_slider.js';
 import Recommend from './components/recommend/recommend.js';
 import Day_rank from './components/rank/day_rank.js';
+import Product_slider from './components/product_slider/product_slider.js';
+import Product_slider_right from './components/product_slider/product_slider_right.js';
 
 class App extends Component {
   render() {
@@ -21,7 +24,15 @@ class App extends Component {
             <Recommend />
             <Day_rank />
           </div>
-          
+          <div className="product_slider">
+            <Product_slider/>
+          </div>
+          <div className="product_slider">
+            <Product_slider_right/>
+          </div>
+          <div className="product_slider">
+            <Product_slider/>
+          </div>
         </React.Fragment>
       </BrowserRouter>
     );
---
+++ src/components/product_slider/product_slider.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import "./product_slider.scss";
+
+
+
+class Product_slider extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+          <React.Fragment>
+            <div className="p_slider_wrap container d-flex align-items-center">
+                <div className="category_pic">
+                    <img src={require("./images/category.jpg")}/>
+                </div>
+                <div className="category_wrap container">
+                    <div className="category_title title1">異國料理</div>
+                    <div className="cards d-flex">
+                        <div className="p_card">
+                            <div className="upper_card">
+                                <img className="card_pic" src ={require("./images/card_pic.jpg")}/>
+                                <div className="rate title1">4.2</div>
+                            </div>
+                            <div className="lower_card">
+                                <div className="card_title title2">蒜香牛小排</div>
+                                <div className="card_text text">15分鐘完成一道健康美味又簡單的料理!</div>
+                                <img className="like_btn" src={require("./images/like.svg")}/>
+                                <img className="share_btn" src={require("./images/share.svg")}/>
+                            </div>
+                        </div>
+                        <div className="p_card">
+                            <div className="upper_card">
+                                <img className="card_pic" src ={require("./images/card_pic.jpg")}/>
+                                <div className="rate title1">4.2</div>
+                            </div>
+                            <div className="lower_card">
+                                <div className="card_title title2">蒜香牛小排</div>
+                                <div className="card_text text">15分鐘完成一道健康美味又簡單的料理!</div>
+                                <img className="like_btn" src={require("./images/like.svg")}/>
+                                <img className="share_btn" src={require("./images/share.svg")}/>
+                            </div>
+                        </div>
+                        <div className="p_card">
+                            <div className="upper_card">
+                                <img className="card_pic" src ={require("./images/card_pic.jpg")}/>
+                                <div className="rate title1">4.2</div>
+                            </div>
+                            <div className="lower_card">
+                                <div className="card_title title2">蒜香牛小排</div>
+                                <div className="card_text text">15分鐘完成一道健康美味又簡單的料理!</div>
+                                <img className="like_btn" src={require("./images/like.svg")}/>
+                                <img className="share_btn" src={require("./images/share.svg")}/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                
+                
+            </div>
+          </React.Fragment>
+        );
+      }
+    }
+
+export default Product_slider;
